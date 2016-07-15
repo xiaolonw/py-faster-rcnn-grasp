cf970a95672724ec36b70935f6df1de9978acf2c
xiaolonw <xiaolonw@cs.cmu.edu>
2016-07-15 01:23:34 -0400

    utils

--- python_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------
-# Python Utils
-# Copyright (c) 2015 UC Berkeley 
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Saurabh Gupta
-# --------------------------------------------------------
