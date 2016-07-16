94f71f8371308e1fc24ef4f0045b0faffbb78d52
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-15 23:55:05 -0400

    orig

---
+++ scripts_data/cpdata_ori.m
@@ -0,0 +1,18 @@
+
+srcdir = '/scratch/xiaolonw/nyud2/data/images/';
+desdir = '/scratch/xiaolonw/nyud3/data/images/'; 
+
+
+list = dir([srcdir '/*.png']); 
+
+for i = 1 : numel(list)
+	fname = list(i).name; 
+	num = str2num(fname(5:8)); 
+	readname = [srcdir '/' fname]; 
+	desname = sprintf('%s/img_%05d.png', desdir, num);
+	cmd = ['cp ' readname ' ' desname]; 
+
+end
+
+
+
