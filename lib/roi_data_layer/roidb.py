52905eb0573082d898b33e3e5c4c9f599421d58f
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-16 03:33:34 -0400

    test

--- lib/roi_data_layer/roidb.py
+++ lib/roi_data_layer/roidb.py
@@ -38,12 +38,12 @@ def prepare_roidb(imdb):
         # sanity checks
         
         # max overlap of 0 => class should be zero (background)
-        # zero_inds = np.where(max_overlaps == 0)[0]
-        # assert all(max_classes[zero_inds] == 0)
+        zero_inds = np.where(max_overlaps == 0)[0]
+        assert all(max_classes[zero_inds] == 0)
 
         # max overlap > 0 => class should not be zero (must be a fg class)
-        # nonzero_inds = np.where(max_overlaps > 0)[0]
-        # assert all(max_classes[nonzero_inds] != 0)
+        nonzero_inds = np.where(max_overlaps > 0)[0]
+        assert all(max_classes[nonzero_inds] != 0)
 
 def add_bbox_regression_targets(roidb):
     """Add information needed to train bounding-box regressors."""
