37d4261364ba8510957fc84587d882b312074c13
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-07-21 13:38:45 -0400

    crate

---
+++ scripts_data/create_gt.m
@@ -0,0 +1,74 @@
+
+src = '/scratch/xiaolonw/grasp_data/benchmarkData/gt_box_cache_dir/'
+bboxdir = '/nfs.yoda/xiaolonw/grasp/dataset/annotations_gray/';
+list = '/nfs.yoda/xiaolonw/grasp/dataset/namelist.txt'; 
+splits = '/scratch/xiaolonw/grasp_data/benchmarkData/metadata/splits.mat';
+
+fid = fopen(list, 'r');
+names = {};
+while ~feof(fid)
+	s = fscanf(fid, '%s', 1);
+	if length(s) == 0
+		break;
+	end
+	names{end + 1} = s;
+
+end
+fclose(fid);
+
+for i = 1 : 10
+	foldername = sprintf('%s/%d', src, i);
+	mkdir(foldername); 
+end
+
+backim = imread('/nfs.yoda/xiaolonw/grasp/dataset/background/background.jpg'); 
+height = size(backim, 1);
+width  = size(backim, 2);
+
+outnames = {};
+
+for i = 1 : numel(names)
+	fname = names{i};
+	boxname = [bboxdir '/' fname '.txt']; 
+	matname = [src '/' fname '.mat'];
+	fid2 = fopen(boxname, 'r');
+
+	rec.objects = [];
+	rec.imgsize = [height, width]; 
+
+	obj.instanceId = 1;
+	obj.difficult = 0;
+	obj.truncated = 0;
+	obj.bbox = [];
+	obj.class = '';
+
+	while ~feof(fid2)
+		cls = fscanf(fid2, '%d', 1);
+		if length(cls) == 0
+			break;
+		end
+		bbox = fscanf(fid2, '%d', 4); 
+		obj.bbox = [bbox(1), bbox(3), bbox(2), bbox(4)]; 
+		obj.class = num2str(cls); 
+		rec.objects = [rec.objects, obj];
+	end
+
+	fclose(fid2); 
+
+	if numel(rec.objects) == 0
+		continue;
+	end
+
+	save(matname, 'rec'); 
+	outnames{end + 1} = fname;
+
+end
+
+rp =  randperm(numel(outnames)); 
+
+trainval = outnames{rp(1 : end - 2000)}; 
+test = outnames{rp(end - 2000 + 1 : end)}; 
+
+
+save( splits , 'test' , 'trainval');
+
